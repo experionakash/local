05f1ea19d11690c4f3406e9504df83e1b3e9d706
akash <akash.chandran@experionglobal.com>
2016-10-28 10:41:13 +0530

    ok

---
+++ fact.java
@@ -0,0 +1,15 @@
+import java.io*;
+
+public class fact
+{
+	public static void main(String args[])
+	{
+
+	int x=10;
+
+		for(int i=1;i<=x;i++)
+		{
+			i*=i;
+		}
+		System.out.println("factorial of"+x+"="i);
+	}
